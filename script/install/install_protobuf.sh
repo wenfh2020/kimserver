de1dbfe90788a9ee74118ab7c1e4625bb3ee4c22
wenfh2020 <wenfh2020@126.com>
2020-08-14 15:38:15 +0800

    update install protobuf script.

--- script/install/install_protobuf.sh
+++ script/install/install_protobuf.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 
+work_path=$(dirname $0)
+cd $work_path
+work_path=$(pwd)
+
 file=protobuf-all-4.0.0-rc-2.tar.gz
 url=https://github.com/protocolbuffers/protobuf/releases/download/v4.0.0-rc2/$file
+
+insert='i\
+if test \"x${ac_cv_env_CFLAGS_set}\" = \"x\"; then : \
+    CFLAGS=\"-fPIC\" \
+fi \
+if test \"x${ac_cv_env_CXXFLAGS_set}\" = \"x\"; then : \
+    CXXFLAGS=\"-fPIC\" \
+fi
+'
+
+insert_func() {
+    if [ $(uname -s) == "Darwin" ]; then
+        sed -i "" "2665,2670d" configure
+        sed -i "" "2665$insert" configure
+    else
+        sed -i "2665,2670d" configure
+        sed -i "2665$insert" configure
+    fi
+}
 
 wget -c $url
 [ $? -ne 0 ] && echo 'download failed!' && exit 1
 tar zxf $file
-cp protobuf_configure protobuf-4.0.0-rc-2
 cd protobuf-4.0.0-rc-2
-mv protobuf_configure configure
 ./configure --disable-shared
-make clean
-make uninstall
-make -j8 && make install
+insert_func
+make clean && make uninstall && make -j8 && make install
 [ $? -ne 0 ] && exit 1
 [ $(uname -s) == "Linux" ] && ldconfig
 protoc --version
