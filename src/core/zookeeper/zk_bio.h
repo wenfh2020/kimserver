93f63f8d64d26ca5b84b871fce11b14341b10b0e
wenfh2020 <wenfh2020@126.com>
2020-11-06 10:05:17 +0800

    optimize code.

--- src/core/zookeeper/zk_bio.h
+++ src/core/zookeeper/zk_bio.h
@@ -20,10 +20,14 @@ class Bio {
     Bio(Log* logger);
     virtual ~Bio();
 
+    /* create a new thread. */
     bool bio_init();
+    /* stop thread. */
     void bio_stop() { m_stop_thread = true; }
 
+    /* add async cmd task to handle in bio thread. */
     bool add_cmd_task(const std::string& path, zk_task_t::CMD cmd, const std::string& value = "");
+    /* add async ack to handle in main thread (timer.) */
     void add_ack_task(zk_task_t* task);
 
     /* bio thread. */
@@ -40,11 +44,12 @@ class Bio {
 
    protected:
     Log* m_logger = nullptr;
-    pthread_t m_thread;
-    volatile bool m_stop_thread = false;
 
+    pthread_t m_thread;
     pthread_cond_t m_cond;
     pthread_mutex_t m_mutex;
+
+    volatile bool m_stop_thread = false;
     std::list<zk_task_t*> m_req_tasks;
     std::list<zk_task_t*> m_ack_tasks;
 };
