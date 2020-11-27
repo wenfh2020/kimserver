e33126a19a46d1338834615645c07cf4aaa6b8b7
wenfh2020 <wenfh2020@126.com>
2020-11-27 09:54:56 +0800

    optimize gen proto script.

--- src/core/protobuf/gen_proto.sh
+++ src/core/protobuf/gen_proto.sh
@@ -3,12 +3,14 @@
 work_path=$(dirname $0)
 cd $work_path
 work_path=$(pwd)
+subdirs=$(ls $work_path)
 
 protoc --version >/dev/null 2>&1
 [ $? -ne 0 ] && echo 'pls install protobufs.' && exit 1
 
-cd $work_path/proto
-protoc -I. --cpp_out=. http.proto msg.proto
-
-cd $work_path/sys
-protoc -I. --cpp_out=. nodes.proto
+for dir in $subdirs; do
+    if [ -d $work_path/$dir ]; then
+        cd $work_path/$dir
+        protoc -I. --cpp_out=. *.proto
+    fi
+done
